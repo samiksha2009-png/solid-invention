9cb17205f12c214c2283ec87b6b05479f3d85099
samiksha2009-png <samikhshanaykodi@gmail.com>
2026-06-29 15:24:49 +0530

    Implement subtraction functionality in Substraction.c
    Fixes include directive and adds subtraction logic.
    C=a-b

---
+++ Substraction.c
@@ -0,0 +1,13 @@
+#incluee<iostream.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+cout<<"enter value of a";
+cin>>a;
+cout<<"enter value of b";
+cin>>b;
+cout<<"sub="<<c;
+getch();
+}
